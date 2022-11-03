7f344ac912996ff3c2c764eaa8e9b841713b0b51
The DotNet Office <muneshs16@gmail.com>
2022-11-03 12:53:13 +0530

    Add files via upload

---
+++ 21_dotnetoffice.component.spec.ts
@@ -0,0 +1,45 @@
+import { HttpClientModule } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { AppRoutingModule } from '../app-routing.module';
+import { Addition } from '../Calculator';
+import { StudentService } from '../services/student.service';
+
+import { DotnetofficeComponent } from './dotnetoffice.component';
+
+describe('DotnetofficeComponent', () => {
+  let component: DotnetofficeComponent;
+  let fixture: ComponentFixture<DotnetofficeComponent>;
+  let h1: HTMLElement;
+  let deb: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DotnetofficeComponent],
+      providers: [StudentService],
+      imports: [AppRoutingModule, HttpClientModule]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(DotnetofficeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    deb = fixture.debugElement;
+
+    h1 = fixture.nativeElement.querySelector('h1');
+  });
+
+  it("call private method", () => {
+    component["ShowName"]();
+    expect(component["Name"]).toEqual("DotNet Office")
+  });
+
+  it("call private method - 1", () => {
+    component["calculate"](10, 20);
+    expect(component.sum).toEqual(30);
+  });
+
+
+});
